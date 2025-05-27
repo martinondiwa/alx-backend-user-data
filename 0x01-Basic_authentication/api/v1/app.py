8b5d28fc4884cb300e0d85c5bfc19d441db51e9d
martinondiwa <surekondiwa@gmail.com>
2025-05-27 18:04:52 +0000

    asd

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,14 +3,14 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import views
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(views)
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
