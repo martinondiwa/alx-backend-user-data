9e8f5e68a3f4bc0a0d790045f6ded3aa366e166d
martinondiwa <surekondiwa@gmail.com>
2025-05-29 09:46:15 +0000

    asd

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -2,6 +2,8 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
+from flask import jsonify
+from api.v1.views import app_views
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
@@ -10,3 +12,8 @@ from api.v1.views.users import *
 from api.v1.views.session_auth import *
 
 User.load_from_file()
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Return status"""
+    return jsonify({"status": "OK"})
